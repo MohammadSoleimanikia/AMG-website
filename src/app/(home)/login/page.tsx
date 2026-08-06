48fa41494849a1f48f285b5dbf424a6742df91f2
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2026-08-06 14:29:31 +0330

    remove .env

---
+++ src/app/(home)/login/page.tsx
@@ -0,0 +1,13 @@
+import { Container, Typography } from '@mui/material';
+
+export default function Page() {
+  return (
+    <div>
+      <div className="w-full bg-primary-main py-9">
+        <Container maxWidth="xxl">
+          <Typography className='text-common-white' variant="h1">ورود و عضویت</Typography>
+        </Container>
+      </div>
+    </div>
+  );
+}
