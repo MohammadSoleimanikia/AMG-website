40ba7286be5a91acdb7bfe13e5c4d5df7809bd00
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2026-08-07 18:49:07 +0330

    add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import { Button, Typography } from '@mui/material';
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <div className="flex w-full flex-col items-center justify-center gap-5 !bg-common-white px-5 py-10 xl:min-h-[calc(100vh-176px)]">
+      <Image
+        src="/images/not-found.webp"
+        alt="not found pic"
+        width={600}
+        height={450}
+        className="h-auto w-full max-w-[650px] object-contain"
+      />
+
+      <Typography variant="body1">
+        صفحه مورد نظر یافت نشد
+      </Typography>
+
+      <Button component={Link} href="/" variant="contained">
+        بازگشت به خانه
+      </Button>
+    </div>
+  );
+}
