9da589cfcca186dab1830b654f2293e046b95798
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2026-08-07 18:50:31 +0330

    change header size

---
+++ src/layout/main/nav/index.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import CartButton from '@/components/cartButton';
+import MobileNavBurgerMenu from '@/layout/main/nav/mobileNavBurgerMenu';
+import NavButtonDesktop from '@/layout/main/nav/navBtnDesktop';
+
+import { Container, Slide, useScrollTrigger } from '@mui/material';
+import RegisterButton from '../header/components/registerButton';
+import { HeaderData } from '@/_types/_header';
+
+export default function NavBar({ headerData }: { headerData: HeaderData }) {
+  const isNavHidden = useScrollTrigger();
+  return (
+    <div className="relative z-10 h-header-mobile ">
+      <Slide
+        in={!isNavHidden}
+        timeout={350}
+        direction="down"
+        unmountOnExit
+        appear={false}
+      >
+        <nav className="z-[1000] h-full w-full bg-background-paper shadow-s0 xl:h-navbar">
+          <Container maxWidth="xxl" className="h-full">
+            <div className="flex h-full items-center justify-between gap-4">
+              {/* desktop nav */}
+              <div className="hidden min-w-0 flex-1 items-center gap-2 xl:flex">
+                {headerData.menu.map((menuItem) => (
+                  <NavButtonDesktop key={menuItem.id} navItem={menuItem} />
+                ))}
+              </div>
+
+              {/* mobile nav */}
+              <div className="xl:hidden">
+                <MobileNavBurgerMenu headerData={headerData} />
+              </div>
+
+              <div className="hidden xl:block">
+                <CartButton />
+              </div>
+
+              <div className="h-full xl:hidden">
+                <RegisterButton className="h-full" />
+              </div>
+            </div>
+          </Container>
+        </nav>
+      </Slide>
+    </div>
+  );
+}
