ac9ab8adab1eb16b4bbbef4853f14781ce80800f
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2026-08-08 08:56:50 +0330

    add 500 status code

--- src/_types/_bsResponse.ts
+++ src/_types/_bsResponse.ts
@@ -1,6 +1,6 @@
 export type BaseResponse<T> = {
   data: T|null;
-  statusCode: 200 | 401 | 404 | 402 | 403 | 405;
+  statusCode: 200 | 401 | 404 | 402 | 403 | 405 | 500;
   message: string;
   success: boolean;
   errors: null | "";
